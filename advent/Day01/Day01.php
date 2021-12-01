81bb27ef6ab86c8bc6f1844dcb0cd5cdb1cb15c8
Mike Roquemore <mikeroq@gmail.com>
2021-11-30 23:34:07 -0600

    Day 1 complete.

--- advent/Day01/Day01.php
+++ advent/Day01/Day01.php
@@ -6,18 +6,52 @@ use pwnstar\AdventOfCode2021\Day;
 
 class Day01 extends Day
 {
+    private int $increaseCount = 0;
+    private array $sumArray = [];
+
     protected function formatInput(): void
     {
-        
+        $this->explodeInputByNewLine();
+    }
+
+    protected function findIncreased($input): void
+    {
+        foreach ($input as $key=>$depth) {
+            if ($key == 0) {
+                continue;
+            } else {
+                if ($depth > $input[$key-1]) {
+                    $this->increaseCount++;
+                }
+            }
+        }
+    }
+
+    protected function slidingWindowCalc(): void
+    {
+        $this->increaseCount = 0;
+        foreach ($this->input as $key=>$depth) {
+            if ($key == 0) {
+                continue;
+            } elseif ($key == count($this->input)-1) {
+                break;
+            } else {
+                @$sum = $this->input[$key-1] + $depth + $this->input[$key+1];
+                array_push($this->sumArray, $sum);
+            }
+        }
+        $this->findIncreased($this->sumArray);
     }
 
     public function findFirstAnswer(): int
     {
-
+        $this->findIncreased($this->input);
+        return $this->increaseCount;
     }
 
     public function findSecondAnswer(): int
     {
-
+        $this->slidingWindowCalc();
+        return $this->increaseCount;
     }
 }
--- advent/Day01/Day01Test.php
+++ advent/Day01/Day01Test.php
@@ -5,11 +5,35 @@ use PHPUnit\Framework\TestCase;
 
 final class Day01Test extends TestCase
 {
-    public function testPart1Equals0(): void
+    public function testPart1ExampleEquals7(): void
     {
         $day01 = new Day01();
         $day01->importInput('advent/Day01/test_input.txt');
 
-        $this->assertEquals(0, $day01->findFirstAnswer());
+        $this->assertEquals(7, $day01->findFirstAnswer());
+    }
+
+    public function testPart2ExampleEquals5(): void
+    {
+        $day01 = new Day01();
+        $day01->importInput('advent/Day01/test_input.txt');
+
+        $this->assertEquals(5, $day01->findSecondAnswer());
+    }
+
+    public function testPart1RealAnswerEquals1713(): void
+    {
+        $day01 = new Day01();
+        $day01->importInput('advent/Day01/input.txt');
+
+        $this->assertEquals(1713, $day01->findFirstAnswer());
+    }
+
+    public function testPart2RealAnswerEquals1734(): void
+    {
+        $day01 = new Day01();
+        $day01->importInput('advent/Day01/input.txt');
+
+        $this->assertEquals(1734, $day01->findSecondAnswer());
     }
 }
