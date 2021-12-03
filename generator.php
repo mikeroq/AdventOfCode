ad0309fbfb727a8cab2e7feb0be03d4d10a3f59e
Mike Roquemore <mikeroq@gmail.com>
2021-12-02 20:41:27 -0600

    Added generator to git

---
+++ generator.php
@@ -0,0 +1,24 @@
+<?php
+
+$year = "2021";
+
+
+$classFile = file_get_contents('stubs/Day.php.stub');
+$testClassFile = file_get_contents('stubs/DayTest.php.stub');
+$readmeFile = file_get_contents('stubs/README.MD.stub');
+
+$days = ['01','02','03','04','05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+
+foreach ($days as $day) {
+    $class = str_replace(['%DAY%', '%YEAR%'], [$day, $year], $classFile);
+    $test = str_replace(['%DAY%', '%YEAR%'], [$day, $year], $testClassFile);
+    $readme = str_replace(['%DAY%', '%YEAR%'], [$day, $year], $readmeFile);
+
+    mkdir("advent/Day$day");
+
+    file_put_contents("advent/Day$day/Day$day.php", $class);
+    file_put_contents("advent/Day$day/Day".$day."Test.php", $test);
+    file_put_contents("advent/Day$day/README.MD", $readme);
+    file_put_contents("advent/Day$day/input.txt", '');
+    file_put_contents("advent/Day$day/test_input.txt", '');
+}
