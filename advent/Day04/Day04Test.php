175ea4b077527a56af320e77cd19f66e8b114ac0
Mike Roquemore <mikeroq@gmail.com>
2021-12-04 00:35:53 -0600

    add result, tests, day 4

--- advent/Day04/Day04Test.php
+++ advent/Day04/Day04Test.php
@@ -20,4 +20,20 @@ final class Day04Test extends TestCase
 
         $this->assertEquals(1924, $day04->findSecondAnswer());
     }
+
+    public function testPart1RealAnswerEquals10680(): void
+    {
+        $day04 = new Day04();
+        $day04->importInput('advent/Day04/input.txt');
+
+        $this->assertEquals(10680, $day04->findFirstAnswer());
+    }
+
+    public function testPart2RealAnswerEquals31892(): void
+    {
+        $day04 = new Day04();
+        $day04->importInput('advent/Day04/input.txt');
+
+        $this->assertEquals(31892, $day04->findSecondAnswer());
+    }
 }
