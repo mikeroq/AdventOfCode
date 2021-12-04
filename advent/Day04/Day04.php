75da2c6edaeea0680631b376b0c80caae473628c
Mike Roquemore <mikeroq@gmail.com>
2021-12-04 00:33:48 -0600

    day 4 solved with spaghetti code

--- advent/Day04/Day04.php
+++ advent/Day04/Day04.php
@@ -6,18 +6,113 @@ use pwnstar\AdventOfCode2021\Day;
 
 class Day04 extends Day
 {
+    private array $numbers = [];
+    private array $calledNumbers = [];
+    private array $cards = [];
+
     protected function formatInput(): void
     {
-        
+        $this->explodeInputByBlankLines();
+        foreach ($this->input as $key => $line) {
+            // line one is numbers
+            if ($key == 0) {
+                $this->numbers = array_map('intval',explode(',', $line));
+            } else {
+                $array = [];
+                foreach(explode("\r\n", trim($line)) as $bingoLine) {
+                    array_push($array, array_map("intval",explode(" ",trim(preg_replace('/\s+/', ' ',$bingoLine)))));
+                }
+                array_push($this->cards, $array);
+            }
+        }
+    }
+
+    protected function hasBingo(array $board): bool
+    {
+        foreach ($board as $row) {
+            if (count(array_filter($row, fn ($num) => in_array($num, $this->calledNumbers))) == 5) {
+                return true;
+            }
+        }
+        for ($col = 1; $col <= count($board[0]); $col++) {
+            if (count(array_filter(array_column($board, $col), fn ($num) => in_array($num, $this->calledNumbers))) == 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function sumUnmarked($card): int
+    {
+        $sum = 0;
+        foreach($card as $row) {
+            foreach ($row as $col) {
+                if (!in_array($col, $this->calledNumbers)) {
+
+                    $sum += $col;
+                }
+            }
+        }
+        return $sum;
+    }
+
+    protected function doPart1(): int
+    {
+        $justCalled = 0;
+        $boardSum = 0;
+        foreach($this->numbers as $callout) {
+            array_push($this->calledNumbers, $callout);
+            foreach ($this->cards as $card) {
+                if ($this->hasBingo($card) == true) {
+                    $justCalled = $callout;
+                    $boardSum = $this->sumUnmarked($card);
+                    return $justCalled * $boardSum;
+                }
+            }
+        }
+        return 0;
+    }
+
+    protected function doPart2(): int
+    {
+        $lastBingo = ['card' => 0, 'callout' => 0];
+        $justCalled = 0;
+        $boardSum = 0;
+        $boardsWon = [];
+        foreach($this->numbers as $callout) {
+            array_push($this->calledNumbers, $callout);
+            foreach ($this->cards as $key => $card) {
+                if(!in_array($key, $boardsWon)) {
+                    if ($this->hasBingo($card) == true) {
+                        if (in_array($key, $boardsWon)) {
+                            break;
+                        } else {
+                            array_push($boardsWon, $key);
+                            $lastBingo['card'] = $key;
+                        }
+                    }
+                }
+            }
+        }
+        $this->calledNumbers = [];
+        foreach($this->numbers as $called) {
+            array_push($this->calledNumbers, $called);
+            if ($this->hasBingo($this->cards[$lastBingo['card']]) == true) {
+                $lastBingo['callout'] = $called;
+                $sum = $this->sumUnmarked($this->cards[$lastBingo['card']]);
+                return $sum * $lastBingo['callout'];
+            }
+        }
+        return 0;
     }
 
     public function findFirstAnswer(): int
     {
-        return 0;
+        return $this->doPart1();
     }
 
     public function findSecondAnswer(): int
     {
-        return 0;
+        return $this->doPart2();
     }
 }
--- advent/Day04/Day04Test.php
+++ advent/Day04/Day04Test.php
@@ -5,11 +5,19 @@ use PHPUnit\Framework\TestCase;
 
 final class Day04Test extends TestCase
 {
-    public function testPart1Equals0(): void
+    public function testPart1ExampleEquals4512(): void
     {
         $day04 = new Day04();
         $day04->importInput('advent/Day04/test_input.txt');
 
-        $this->assertEquals(0, $day04->findFirstAnswer());
+        $this->assertEquals(4512, $day04->findFirstAnswer());
+    }
+
+    public function testPart2ExampleEquals1924(): void
+    {
+        $day04 = new Day04();
+        $day04->importInput('advent/Day04/test_input.txt');
+
+        $this->assertEquals(1924, $day04->findSecondAnswer());
     }
 }
