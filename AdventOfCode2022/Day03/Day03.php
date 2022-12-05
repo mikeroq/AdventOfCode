cdc67f63e8afb1a2a622d7142d8dc34bb7d9f76c
mikeroq <mikeroq@gmail.com>
2022-12-04 20:01:23 -0600

    Day 3

--- AdventOfCode2022/Day03/Day03.php
+++ AdventOfCode2022/Day03/Day03.php
@@ -6,18 +6,56 @@ use mikeroq\AdventOfCode\Shared\Day;
 
 class Day03 extends Day
 {
+
     protected function formatInput(): void
     {
-        
+        $this->explodeInputByNewLine();
+    }
+
+    private function getPriority($letter): int
+    {
+        return ctype_lower($letter) ? ord($letter) - ord('a') + 1 : ord($letter) - ord('A') + 27;
+    }
+
+    private function findCommonItemType($rucksacks)
+    {
+        $itemTypes = array_unique(array_merge(...array_map(function ($rucksack) {
+            return str_split($rucksack);
+        }, $rucksacks)));
+        foreach ($itemTypes as $type) {
+            if (count(array_filter($rucksacks, function($rucksack) use ($type) {
+                    return str_contains($rucksack, $type);
+                })) == 3) {
+                return $type;
+            }
+        }
+        return null;
     }
 
     public function findFirstAnswer(): int
     {
-        return 0;
+        $commonItems = array_reduce($this->input, function($carry, $rucksack) {
+            $ruck = str_split($rucksack, strlen($rucksack)/2);
+            $common = array_intersect(array_unique(str_split($ruck[0])), array_unique(str_split($ruck[1])));
+            if (!empty($common)) {
+                $carry[] = $common;
+            }
+            return $carry;
+        }, []);
+
+        return array_reduce($commonItems, function($carry, $commonItem) {
+            return $carry + $this->getPriority(array_shift($commonItem));
+        }, 0);
     }
 
     public function findSecondAnswer(): int
     {
-        return 0;
+        $commonTypes = array_map(function ($group) {
+            return $this->findCommonItemType($group);
+        }, array_chunk($this->input, 3));
+
+        return array_reduce($commonTypes, function ($carry, $type) {
+            return $carry + $this->getPriority($type);
+        }, 0);
     }
 }
