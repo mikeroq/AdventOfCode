0c79b5788d54685b1cea6ce5d0447a36462be208
mikeroq <mikeroq@gmail.com>
2022-12-11 21:36:57 -0600

    Day 10 part 1 solved

--- AdventOfCode2022/Day10/Day10.php
+++ AdventOfCode2022/Day10/Day10.php
@@ -6,14 +6,43 @@ use mikeroq\AdventOfCode\Shared\Day;
 
 class Day10 extends Day
 {
+
     protected function formatInput(): void
     {
-        
+        $this->explodeInputByNewLine();
+    }
+
+    public function processInstructions()
+    {
+        $cycles = [];
+        $start = 1;
+        $xRegister = 1;
+        foreach ($this->input as $key => $cycle) {
+            $cycle = explode(' ', $cycle);
+
+            switch($cycle[0]) {
+                case 'noop':
+                    $cycles[$start] = $xRegister;
+                    $start++;
+                    break;
+                case 'addx':
+                    $cycles[$start] = $xRegister;
+                    $start++;
+                    $cycles[$start] = $xRegister;
+                    $start++;
+                    $xRegister = $xRegister + intval(trim($cycle[1]));
+                    break;
+            }
+        }
+        return $cycles;
     }
 
     public function findFirstAnswer(): int
     {
-        return 0;
+
+        $cycles = $this->processInstructions();
+        return ($cycles[20] * 20) + ($cycles[60] * 60) + ($cycles[100] * 100) + ($cycles[140] * 140) + ($cycles[180] *
+                180) + ($cycles[220] * 220);
     }
 
     public function findSecondAnswer(): int
--- AdventOfCode2022/Day10/Day10Test.php
+++ AdventOfCode2022/Day10/Day10Test.php
@@ -6,12 +6,12 @@ use PHPUnit\Framework\TestCase;
 
 final class Day10Test extends TestCase
 {
-    public function testPart1Equals0(): void
+    public function testPart1ExampleEquals13140(): void
     {
         $day10 = new Day10();
         $day10->importInput('AdventOfCode2022/Day10/test_input.txt');
 
-        $this->assertEquals(0, $day10->findFirstAnswer());
+        $this->assertEquals(13140, $day10->findFirstAnswer());
     }
 
     public function testPart2Equals0(): void
